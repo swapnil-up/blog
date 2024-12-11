9cb0b6e74e33c851c1c5f24ba41fe7ee54c11b8d
swapnil-up <swapnilupadhyaykharel@gmail.com>
2024-12-11 11:43:56 +0545

    Got laravel to work with inertia to connect to the vue frontend

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+    Hello {{ counter }}!
+
+    <div><increment /></div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import increment from "../components/increment.vue";
+
+const counter = ref(0);
+setInterval(() => counter.value++, 1000);
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::view('/','homepage');
-Route::view('/about','about');
-Route::view('/articles/{id}','articles');
-Route::get('/test',function(){
+Route::view('/', 'homepage');
+Route::view('/about', 'about');
+Route::view('/articles/{id}', 'articles');
+Route::get('/test', function () {
     return 'Hello, Laravel is working!';
+});
+Route::get('/testing', function () {
+    return inertia('Welcome');
 });
